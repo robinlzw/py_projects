ac0c843455041982122bba75966decf26ec59fbc
zhenwei.li <lizhenwei@chinaredstar.cn>
2020-09-18 17:22:05 +0800

    优化代码

--- Scripts/convert_file_encoding.py
+++ Scripts/convert_file_encoding.py
@@ -6,10 +6,8 @@ import re
 import chardet
 
 convert_dir = sys.argv[1]
-convert_file_types = [".txt"]
+convert_file_types = [".cpp", ".h", ".hpp"]
 
-
-# convertfiletypes = [".cpp", ".h", ".hpp"]
 
 def convert_encoding(filename, target_encoding):
     # Backup the origin file.
@@ -17,10 +15,11 @@ def convert_encoding(filename, target_encoding):
     # convert file from the source encoding to target encoding
     content = codecs.open(filename, 'rb').read()
     source_encoding = chardet.detect(content)['encoding']
-    print(source_encoding, filename)
+
+    # if source_encoding != 'utf-8':
     if source_encoding != 'utf-8':
         print(source_encoding, filename)
-        content = content.decode(source_encoding, 'ignore')  # .encode(source_encoding)
+        content = content.decode(source_encoding, 'ignore')
         codecs.open(filename, 'w', encoding=target_encoding).write(content)
 
 
--- Scripts/find_file_recurse.py
+++ Scripts/find_file_recurse.py
@@ -8,6 +8,7 @@ import time
 import re
 
 rmFileName = []
+file_types = [".cpp", ".h"]
 
 
 def usage():
@@ -21,12 +22,13 @@ def listDir(fileDir, year, b_delete, to_find_str):
             ft = os.stat(fileDir + "/" + eachFile)
             ltime = time.localtime(int(ft.st_mtime))  # 获取文件最后修改时间
             file_year = time.strftime("%Y", ltime)
-            # if int(file_year) == int(year):
 
             if eachFile not in rmFileName:
-                if "txt" in eachFile.rsplit('.', 1)[1]:
-                    with open(fileDir + "/" + eachFile, 'r', encoding='utf8') as f:
-                        find_str_in_file(f, to_find_str)
+                with open(fileDir + "/" + eachFile, 'r', encoding='utf8') as f:
+                    foundLine = find_str_in_file(f, to_find_str)
+                    if foundLine != "!!!???":
+                        print("****" + foundLine + "####" + fileDir + "/" + eachFile)
+
                 if b_delete:
                     print("删除文件: " + fileDir + "//" + eachFile)
                     # os.remove(fileDir + "/" + eachFile);  # 删除文件
@@ -47,33 +49,40 @@ def del_emp_dir(path):
 
 def find_str_in_file(file, to_find_str):
     for line in file.readlines():
-        # line = line.strip("\n")
-        try:
-            #             print(line)
-            #             print(line.decode('utf8'))
-            line.decode('utf8')
-            # 为了暴露出错误，最好此处不print
+        # try:
+        #     #             print(line)
+        #     #             print(line.decode('utf8'))
+        #     line.decode('utf8')
+        #     # 为了暴露出错误，最好此处不print
+        #
+        # except:
+        #     #print("to_find_str not in line")
+        #     not_print_str = "not print"
 
-        except:
-            #print("to_find_str not in line")
-            not_print_str = "not print"
         if to_find_str in line:
-            if "展示文件" not in line:
-                print("**************", line)
+            print(line)
+            file.close()
+            return line
+
     file.close()
+    return "!!!???"
+
+
+def list_file_in_dir(input_dir, to_find_str):
+    for root, dirs, files in os.walk(input_dir):
+        for f in files:
+            for fileType in file_types:
+                if f.lower().endswith(fileType):
+                    filename = os.path.join(root, f)
+                    with open(filename) as my_file_handler:
+                        try:
+                            find_str_in_file(my_file_handler, to_find_str)
+                        except Exception as e:
+                            print(e)
 
 
 if __name__ == '__main__':
-    # options, args = getopt.getopt(sys.argv[1:], '',['path=','year='])
-    # for name, value in options:
-    #     if name in ('-p','--path'):
-    #         path = format(value)
-    #     elif name in ('-y','--year'):
-    #         year = format(value)
-    #
-    # if 'path' not in locals().keys() or not 'year' in locals().keys() :
-    #     usage()
-    rootPath = "E:/Documents"
+    rootPath = "C:/Users/lenovo/source/git_work_space/slate"
     # openFileName = "E:\work\client_QML_final2\WindowsClient.qrc"
 
     # f = open(openFileName, "r")  # 设置文件对象
@@ -84,7 +93,7 @@ if __name__ == '__main__':
     #         rmFileName.append(fileItem)
     #
     # f.close()  # 关闭文件
-    # print(rmFileName)
-    # listDir(fileDir="E:\work\client_QML_final2\Resources", year=2019)
-    listDir(fileDir=rootPath, year=2019, b_delete=False, to_find_str="qt")
+
+    # listDir(fileDir=rootPath, year=2019, b_delete=False, to_find_str="qt")
+    list_file_in_dir(input_dir=rootPath, to_find_str="panelSplitView")
     # del_emp_dir(path)
